bd4fe1af16eb89a545118c976635dd92a4e22a9c
birdskyws <birdskyws@163.com>
2018-10-24 14:52:07 +0800

    project root

---
+++ src/main/java/mooc/birdsky/concurrency/ConcurrencyApplication.java
@@ -0,0 +1,12 @@
+package mooc.birdsky.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyApplication.class, args);
+	}
+}
